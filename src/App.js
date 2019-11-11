c010b3139a3df045f5b9e3fd7e41d3e709e0387f
Tommy <tommyxu0814@gmail.com>
2019-11-10 19:03:40 -0800

    Header Component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 
 const HatsPage = () => (
   <div>
@@ -15,6 +16,7 @@ const HatsPage = () => (
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
