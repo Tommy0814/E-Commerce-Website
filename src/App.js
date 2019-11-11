33f58ee2c08709272e278d59e38fee36c21c9516
Tommy <tommyxu0814@gmail.com>
2019-11-10 17:12:24 -0800

    Section5: 65

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
