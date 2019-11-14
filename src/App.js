70271525fdde013b1753d78b717117606bab6a6f
DevanshiSukhadia <devisukha12@gmail.com>
2019-11-14 11:37:08 -0800

    styled component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
