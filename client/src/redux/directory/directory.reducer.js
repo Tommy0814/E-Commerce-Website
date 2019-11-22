cb156c37b241b5f664fd9d6b23397feca4ff2078
Tommy <tommyxu0814@gmail.com>
2019-11-22 13:21:07 -0800

    Update Home Page Images

--- client/src/redux/directory/directory.reducer.js
+++ client/src/redux/directory/directory.reducer.js
@@ -2,32 +2,32 @@ const INITIAL_STATE = {
   sections: [
     {
       title: 'hats',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+      imageUrl: 'https://cdn.wallpapersafari.com/88/11/Mrbj1w.jpg',
       id: 1,
       linkUrl: 'shop/hats'
     },
     {
       title: 'jackets',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+      imageUrl: 'https://static2.bigstockphoto.com/0/5/1/large1500/150406364.jpg',
       id: 2,
       linkUrl: 'shop/jackets'
     },
     {
       title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+      imageUrl: 'https://d1yn1kh78jj1rr.cloudfront.net/image/preview/rDtN98Qoishumwih/graphicstock-pair-of-red-sneakers-on-a-floor-studio-shot-on-a-wooden-background_B0ZSSQ6W-_SB_PM.jpg',
       id: 3,
       linkUrl: 'shop/sneakers'
     },
     {
       title: 'womens',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+      imageUrl: 'https://www.cowboydatingexpert.com/wp-content/uploads/2019/09/Retro-cowgirl-in-jeans-jacket-in-countryside.jpg',
       size: 'large',
       id: 4,
       linkUrl: 'shop/womens'
     },
     {
       title: 'mens',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+      imageUrl: 'https://s.studiobinder.com/wp-content/uploads/2014/04/Different-Types-of-Shots-Cowboy-Shot-Header.jpeg',
       size: 'large',
       id: 5,
       linkUrl: 'shop/mens'
